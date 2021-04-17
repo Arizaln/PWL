deb1f33a8361fbd1a998cab3eec4307d54f81348
Arizal Nur Islachuddin <72436271+Arizaln@users.noreply.github.com>
2021-04-17 21:11:39 +0700

    Add files via upload

---
+++ appku/footer.php
@@ -0,0 +1,9 @@
+<div class="row">
+	<div class="col-md-12">
+
+    <div class="alert alert-primary" role="alert">
+        Design By: Arizal Nur Islachuddin &copy; 2021
+    </div>
+
+	</div>
+</div>
